d0add9d56a0a4e0c586fcc0ef6472702f672261a
Ciluk <41442866+jesspoex@users.noreply.github.com>
2023-02-05 11:11:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const {Client, GatewayIntentBits}=require('discord.js')
+const {updateBot}=require('./updatePrice')
+const keepAlive = require ("./keepAlive.js");
+
+//This Bot works for tokens listed on coingecko or Solana SPL tokens visible on https://www.jup.ag
+
+
+//IMPORTANT THINGS TO UPDATE---------------------------
+
+//! ADD A KEY NAMED "BOT_TOKEN" (WITHOUT QUOTES...) IN SECRETS ON THE LEFT. THE VALUE SHOULD BE YOUR BOT APPLICATION TOKEN.
+
+//! ADD KEY NAMED "GUILD_ID" IN SECRETS ON THE LEFT. THE VALUE SHOULD BE YOUR DISCORD SERVER ID. ENABLE DEVELOPER OPTIONS (IN YOUR ADVANCED SETTINGS ON DISCORD) AND RIGHT CLICK ON THE SERVER & CLICK COPY ID TO GET THE ID.
+
+const inputs={
+  
+  USE_COINGECKO_PRICING: true, //If USE_COINGECKO_PRICING: false, the bot will use Jupiter Aggregator pricing (Solanatokens only)
+
+  TOKEN_NAME: "BONK", //REQUIRED for Coingecko pricing.  Must match coingecko coin URL name: e.g. solana for https://www.coingecko.com/en/coins/solana. 
+  
+  TOKEN_SYMBOL: "BONK",
+
+  WATCHING: "BONK"  //Change string to define "WATCHING" text.
+};
+
+//-------------------------------------------------------------
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+  ]
+})
+
+client.once('ready', ()=>{
+  console.log("ready yay!")
+
+  updateBot(inputs, client);
+
+  client.user.setActivity(inputs.WATCHING, {type: 'WATCHING'})
+
+  setInterval(function(){
+   updateBot(inputs, client);
+  }, 1000*60*60) //Update price every 60 minutes
+})
+
+//keepAlive();
+
+client.login(process.env.BOT_TOKEN)
+
---
+++ updatePrice.js
@@ -0,0 +1,79 @@
+require('isomorphic-fetch')
+
+//fetch price Coingecko
+async function fetchPrice_coingecko(name){
+  const name_l=name.toLowerCase();
+  const url='https://api.coingecko.com/api/v3/simple/price?ids='+name_l+'&vs_currencies=usd';
+
+  let priceInfo=null;
+  await fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(res_data) {
+      priceInfo=res_data;
+    }).catch(function(err) {
+      console.log('Fetch Error :-S');
+      throw err;
+    });
+
+  if (!priceInfo){return;}
+
+  console.log(priceInfo)
+  
+  return priceInfo[name_l]['usd']
+}
+
+
+//Jupiter fetch price
+async function fetchPrice(symbol){
+
+  const url='https://price.jup.ag/v1/price?id='+symbol
+  /*
+  for BNB tokens use url below. replace symbol with a mint address
+  const url='https://api.pancakeswap.info/api/v2/tokens/'+symbol
+  */
+  let priceInfo=null;
+  await fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(res_data) {
+      priceInfo=res_data;
+    }).catch(function(err) {
+      console.log('Fetch Error :-S');
+      throw err;
+    });
+
+  if (!priceInfo){return;}
+  if (!priceInfo.data){return;}
+  if (!priceInfo.data.price){return;}
+  
+  return priceInfo.data.price
+}
+
+
+  
+//update the bot
+async function updateBot(inputs, client){
+
+  try{
+    let guild=client.guilds.cache.get(process.env.GUILD_ID)
+
+    let member = guild.members.cache.get(client.user.id)
+  
+    if (inputs.USE_COINGECKO_PRICING){
+      let price=await fetchPrice_coingecko(inputs.TOKEN_NAME);
+      member.setNickname(inputs.TOKEN_SYMBOL+': $'+price+' USD');
+      
+    } else {
+      let price=await fetchPrice(inputs.TOKEN_SYMBOL)
+      member.setNickname(inputs.TOKEN_SYMBOL+': $'+price+' USD');
+      
+    }
+  } catch(e){
+    console.log(e)
+  }
+
+  return
+}
+
+module.exports={
+  updateBot
+}
